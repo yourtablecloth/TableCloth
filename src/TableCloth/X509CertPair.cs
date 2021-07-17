ef4f08abee82b856110a552e6782d32fff8f7cb3
level120 <tnara9573@gmail.com>
2021-07-17 18:34:18 +0900

    xml document 구현 부분을 xml serialization으로 변경

--- src/TableCloth/X509CertPair.cs
+++ src/TableCloth/X509CertPair.cs
@@ -6,65 +6,64 @@ using System.Security.Cryptography.X509Certificates;
 
 namespace TableCloth
 {
-    public sealed class X509CertPair
+    public sealed partial class X509CertPair
     {
-		public static IEnumerable<X509CertPair> ScanX509CertPairs()
-		{
-			var list = new List<X509CertPair>();
-            var directoryCandidates = new List<string>
-            {
-                Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "AppData", "LocalLow", "NPKI"),
-            };
-
-            foreach (var eachDrive in DriveInfo.GetDrives())
-			{
-				if (eachDrive.DriveType != DriveType.Removable)
-					continue;
-
-				directoryCandidates.Add(eachDrive.RootDirectory.FullName);
-			}
-
-			foreach (var eachDirectoryCandidate in directoryCandidates)
-				if (Directory.Exists(eachDirectoryCandidate))
-					list.AddRange(ScanX509CertPairs(eachDirectoryCandidate));
-
-			return list.ToArray();
-		}
-
-		// https://stackoverflow.com/questions/5098011/directory-enumeratefiles-unauthorizedaccessexception
-		public static IEnumerable<X509CertPair> ScanX509CertPairs(string rootPath)
-		{
-			var foundFiles = Enumerable.Empty<X509CertPair>();
-
-			try
-			{
-				IEnumerable<string> subDirs = Directory.EnumerateDirectories(rootPath);
-				foreach (string dir in subDirs)
-				{
-					// Add files in subdirectories recursively to the list
-					foundFiles = foundFiles.Concat(ScanX509CertPairs(dir));
-				}
-			}
-			catch (UnauthorizedAccessException) { }
-			catch (PathTooLongException) { }
-
-			try
-			{
-				// Add files from the current directory
-				var singleDerFile = Directory.EnumerateFiles(rootPath, "*.der").FirstOrDefault();
-				var singleKeyFile = Directory.EnumerateFiles(rootPath, "*.key").FirstOrDefault();
-
-				if (File.Exists(singleDerFile) && File.Exists(singleKeyFile))
-					foundFiles = foundFiles.Concat(new X509CertPair[] { CreateX509CertPair(singleDerFile, singleKeyFile) });
-			}
-			catch (UnauthorizedAccessException) { }
-
-			return foundFiles;
-		}
-
-		public static X509CertPair CreateX509CertPair(string derFilePath, string keyFilePath)
+        public static IEnumerable<X509CertPair> ScanX509CertPairs()
         {
-			if (!File.Exists(derFilePath))
+            var defaultNpkiPath = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "AppData", "LocalLow", "NPKI");
+
+            var directoryCandidates = new List<string> {defaultNpkiPath};
+
+            var usbDrives = DriveInfo.GetDrives()
+                .Where(d => d.DriveType == DriveType.Removable)
+                .Select(d => d.RootDirectory.FullName);
+
+            directoryCandidates.AddRange(usbDrives);
+
+            return directoryCandidates.Where(Directory.Exists).SelectMany(ScanX509CertPairs);
+
+        }
+
+        // https://stackoverflow.com/questions/5098011/directory-enumeratefiles-unauthorizedaccessexception
+        public static ICollection<X509CertPair> ScanX509CertPairs(string rootPath)
+        {
+            var foundFiles = new List<X509CertPair>();
+
+            try
+            {
+                foreach (var dir in Directory.EnumerateDirectories(rootPath))
+                {
+                    // Add files in subdirectories recursively to the list
+                    foundFiles.AddRange(ScanX509CertPairs(dir));
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+            }
+            catch (PathTooLongException)
+            {
+            }
+
+            try
+            {
+                // Add files from the current directory
+                var singleDerFile = Directory.EnumerateFiles(rootPath, "*.der").FirstOrDefault();
+                var singleKeyFile = Directory.EnumerateFiles(rootPath, "*.key").FirstOrDefault();
+
+                if (File.Exists(singleDerFile) && File.Exists(singleKeyFile))
+                    foundFiles.Add(CreateX509CertPair(singleDerFile, singleKeyFile));
+            }
+            catch (UnauthorizedAccessException)
+            {
+            }
+
+            return foundFiles;
+        }
+
+        public static X509CertPair CreateX509CertPair(string derFilePath, string keyFilePath)
+        {
+            if (!File.Exists(derFilePath))
                 throw new FileNotFoundException("Certification file (.der) does not exists.", derFilePath);
 
             if (!File.Exists(keyFilePath))
@@ -77,25 +76,27 @@ namespace TableCloth
                 .Split(',', StringSplitOptions.RemoveEmptyEntries)
                 .Select(x =>
                 {
-                    var parts = x.Trim().Split('=', StringSplitOptions.None);
+                    var parts = x.Trim().Split('=');
                     var unitName = parts.ElementAtOrDefault(0)?.Trim() ?? string.Empty;
                     var value = parts.ElementAtOrDefault(1)?.Trim() ?? string.Empty;
                     return new KeyValuePair<string, string>(unitName, value);
-                });
+                })
+                .ToArray();
+
             var organizationName = subjectNamePairs
                 .Where(x => string.Equals(x.Key, "o", StringComparison.InvariantCultureIgnoreCase))
                 .Select(x => x.Value)
                 .FirstOrDefault();
-            var usageExtension = cert.Extensions
-                .Cast<X509Extension>()
-                .Where(x => x is X509KeyUsageExtension)
-                .Select(x => x as X509KeyUsageExtension)
-                .FirstOrDefault();
-            var isPersonalCert = usageExtension != null &&
-                usageExtension.KeyUsages.HasFlag(X509KeyUsageFlags.NonRepudiation) &&
-                usageExtension.KeyUsages.HasFlag(X509KeyUsageFlags.DigitalSignature);
 
-            return new X509CertPair()
+            var usageExtension = cert.Extensions
+                .OfType<X509KeyUsageExtension>()
+                .FirstOrDefault();
+
+            var isPersonalCert = usageExtension != null &&
+                                 usageExtension.KeyUsages.HasFlag(X509KeyUsageFlags.NonRepudiation) &&
+                                 usageExtension.KeyUsages.HasFlag(X509KeyUsageFlags.DigitalSignature);
+
+            return new X509CertPair
             {
                 Subject = subjectNamePairs.ToArray(),
                 IsPersonalCert = isPersonalCert,
@@ -103,8 +104,11 @@ namespace TableCloth
                 KeyFilePath = keyFilePath,
             };
         }
+    }
 
-		private X509CertPair() { }
+    public partial class X509CertPair
+    {
+        private X509CertPair() { }
 
         public string DerFilePath { get; init; }
         public string KeyFilePath { get; init; }
