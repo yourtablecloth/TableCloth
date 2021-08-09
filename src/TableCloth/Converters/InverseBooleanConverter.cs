ed6b0dd2d98edacbf1683ffc556060fe6ffae714
Jung Hyun Nam <rkttu@rkttu.com>
2021-08-09 18:43:26 +0900

    인증서 연동 여부에 따라 컨트롤 활성화 여부를 결정하도록 바인딩 추가

---
+++ src/TableCloth/Converters/InverseBooleanConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace TableCloth.Converters
+{
+    // https://stackoverflow.com/questions/1039636/how-to-bind-inverse-boolean-properties-in-wpf
+    [ValueConversion(typeof(bool), typeof(bool))]
+    public class InverseBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (targetType != typeof(bool))
+                throw new InvalidOperationException("The target must be a boolean");
+
+            return !(bool)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotSupportedException();
+    }
+}
