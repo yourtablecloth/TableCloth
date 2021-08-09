1fccb301d9863fbde19ca798c4bb0617994e915c
Jung Hyun Nam <rkttu@rkttu.com>
2021-08-09 18:56:37 +0900

    인증서 수동으로 찾기 기능 추가

--- src/TableCloth/CertSelectWindow.xaml.cs
+++ src/TableCloth/CertSelectWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +13,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using TableCloth.Models.Configuration;
+using TableCloth.Resources;
 using TableCloth.ViewModels;
 
 namespace TableCloth.Implementations.WPF
@@ -33,7 +37,48 @@ namespace TableCloth.Implementations.WPF
 
         private void OpenCertPairManuallyButton_Click(object sender, RoutedEventArgs e)
         {
+            if (DataContext is not CertSelectWindowViewModel vm)
+                return;
 
+            var ofd = new OpenFileDialog()
+            {
+                AddExtension = true,
+                CheckFileExists = true,
+                CheckPathExists = true,
+                DereferenceLinks = true,
+                Filter = (string)Application.Current.Resources["CertSelectWindow_FileOpenDialog_FilterText"],
+                FilterIndex = 0,
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
+                Multiselect = true,
+                ReadOnlyChecked = true,
+                ShowReadOnly = false,
+                Title = (string)Application.Current.Resources["CertSelectWindow_FileOpenDialog_Text"],
+                ValidateNames = true,
+            };
+
+            // To Do: NPKI 폴더 외에 이동식 드라이브, 사용자 인증서 폴더 등을 미리 추가할 수 있을지 검토 필요
+            var npkiPath = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "AppData", "LocalLow", "NPKI");
+
+            if (Directory.Exists(npkiPath))
+            {
+                ofd.CustomPlaces = new FileDialogCustomPlace[]
+                {
+                new FileDialogCustomPlace(npkiPath),
+                };
+            }
+
+            var response = ofd.ShowDialog();
+
+            if (response.HasValue && response.Value)
+            {
+                vm.SelectedCertPair = vm.CertPairScanner.CreateX509CertPair(
+                    ofd.FileNames.Where(x => string.Equals(".der", System.IO.Path.GetExtension(x), StringComparison.OrdinalIgnoreCase)).First(),
+                    ofd.FileNames.Where(x => string.Equals(".key", System.IO.Path.GetExtension(x), StringComparison.OrdinalIgnoreCase)).First()
+                );
+                DialogResult = true;
+                Close();
+                return;
+            }
         }
 
         private void OkayButton_Click(object sender, RoutedEventArgs e)
