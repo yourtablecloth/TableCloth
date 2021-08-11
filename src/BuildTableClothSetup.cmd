5aaf478af296112a6644062183f4ddef118091b4
Jung Hyun Nam <rkttu@rkttu.com>
2021-08-12 01:19:41 +0900

    BuildTableClothSetup이 항상 Release 빌드만 만들도록 수정

--- src/BuildTableClothSetup.cmd
+++ src/BuildTableClothSetup.cmd
@@ -1,15 +1,7 @@
 @echo off
 pushd "%~dp0"
 
-choice /t 5 /d R /c DRC /m "D: Debug, R: Release, C: Cancel (Default: R)"
-if %ERRORLEVEL% EQU 1 (
-  "%programfiles(x86)%\NSIS\Bin\makensis.exe" /DBUILD_CONFIG=Debug TableClothSetup.nsi
-  goto exit
-)
-if %ERRORLEVEL% EQU 2 (
-  "%programfiles(x86)%\NSIS\Bin\makensis.exe" /DBUILD_CONFIG=Release TableClothSetup.nsi
-  goto exit
-)
+"%programfiles(x86)%\NSIS\Bin\makensis.exe" /DBUILD_CONFIG=Release TableClothSetup.nsi
 
 :exit
 popd
