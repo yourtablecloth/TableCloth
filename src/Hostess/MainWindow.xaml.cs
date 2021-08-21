17350d266f585d758a3077a9d74daaad040a461b
Jung Hyun Nam <rkttu@rkttu.com>
2021-08-21 21:04:55 +0900

    publicsuffix.org의 최신 데이터를 우선시하도록 변경

--- src/Hostess/MainWindow.xaml.cs
+++ src/Hostess/MainWindow.xaml.cs
@@ -10,7 +10,6 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Automation.Peers;
