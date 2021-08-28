bdfd3bc06c72d46280d3501d300f218326848127
Jung Hyun Nam <jeonghyun.nam@devsisters.com>
2021-08-28 23:43:04 +0900

    MSI 패키지 생성 관련 버그 수정 및 기타 정리 작업

--- src/TableCloth.SetupBuilder/Properties/AssemblyInfo.cs
+++ src/TableCloth.SetupBuilder/Properties/AssemblyInfo.cs
@@ -1,18 +1,22 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TableCloth.SetupBuilder")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TableCloth.SetupBuilder")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTitle("Table Cloth Setup Builder")]
+[assembly: AssemblyDescription("This program is a tool that builds the setup package.")]
+[assembly: AssemblyCompany("rkttu.com")]
+[assembly: AssemblyProduct("Table Cloth")]
+[assembly: AssemblyCopyright("(c) rkttu.com, 2021")]
+[assembly: AssemblyTrademark("Table Cloth")]
 [assembly: AssemblyCulture("")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else // DEBUG
+[assembly: AssemblyConfiguration("Release")]
+#endif // DEBUG
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -31,6 +35,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// [assembly: AssemblyVersion("0.5.0.0")]
+[assembly: AssemblyVersion("0.5.0.0")]
+[assembly: AssemblyFileVersion("0.5.0.0")]
