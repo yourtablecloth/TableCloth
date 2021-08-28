15c5db2b8be2d4efa44248e88c4f8b9d8151394d
Jung Hyun Nam <rkttu@rkttu.com>
2021-08-28 17:03:22 +0900

    설치 시 관리자 권한이 필요 없도록 조정
    - 라이센스 동의 추가

--- src/TableCloth.SetupBuilder/Program.cs
+++ src/TableCloth.SetupBuilder/Program.cs
@@ -8,23 +8,17 @@ namespace TableCloth.SetupBuilder
     {
         private static Project DefineCoreFeature(Project targetProject, string inputDirectory)
         {
-            var coreFeature = new Feature("CoreFeature", true)
-            {
-                AllowChange = false,
-                Display = FeatureDisplay.hidden,
-            };
-
-            var mainDirectory = new Dir(coreFeature, @"%ProgramFiles%\TableCloth");
+            var mainDirectory = new Dir(@"%LocalAppDataFolder%\Program\TableCloth");
 
             var mainFiles = System.IO.Directory
                 .GetFiles(inputDirectory, "*.*", System.IO.SearchOption.AllDirectories)
-                .Select(x => new File(coreFeature, System.IO.Path.GetFullPath(x)))
+                .Select(x => new File(System.IO.Path.GetFullPath(x)))
                 .ToArray();
 
             var mainExecFile = mainFiles.Where(x => string.Equals(System.IO.Path.GetFileName(x.Name), "TableCloth.exe", StringComparison.OrdinalIgnoreCase)).SingleOrDefault();
             mainExecFile.Shortcuts = new FileShortcut[]
             {
-                new FileShortcut("TableCloth", @"%ProgramMenu%") { AttributesDefinition = "Advertise=yes", },
+                new FileShortcut("식탁보", @"%ProgramMenu%") { },
             };
 
             mainDirectory.Files = mainFiles;
@@ -50,8 +44,16 @@ namespace TableCloth.SetupBuilder
             }
 
             var project = new Project("TableCloth");
+            project.Package.AttributesDefinition = "Platform=x64";
             project.GUID = Guid.Parse("1D7A2F0E-550D-452B-B69C-585F87C23A5B");
+            project.UpgradeCode = Guid.Parse("C63DF133-51A9-4139-BD31-EDC025C7EB51");
+            project.Encoding = System.Text.Encoding.UTF8;
             project.OutFileName = "TableCloth";
+            project.LicenceFile = "License.rtf";
+            project.Language = "ko-KR";
+            project.UI = WUI.WixUI_Minimal;
+            project.InstallScope = InstallScope.perUser;
+            project.Name = "식탁보";
 
             project = DefineCoreFeature(project, inputDirectory);
 
