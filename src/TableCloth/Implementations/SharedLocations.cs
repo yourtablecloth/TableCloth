94436abdc0cef083cab65c6c8633f2386ef0251d
Jung Hyun Nam <rkttu@rkttu.com>
2021-11-11 23:44:25 +0900

    #38 이슈에 대한 빠른 해결책을 적용함

--- src/TableCloth/Implementations/SharedLocations.cs
+++ src/TableCloth/Implementations/SharedLocations.cs
@@ -7,7 +7,7 @@ namespace TableCloth.Implementations
     public sealed class SharedLocations : ISharedLocations
     {
         public string AppDataDirectoryPath =>
-            Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "TableCloth");
+            Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), "TableCloth");
 
         public string GetDataPath(string relativePath) =>
             Path.Combine(AppDataDirectoryPath, relativePath);
