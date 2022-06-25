2b7d4c2bcb30db6a38d3881bb6c8ab46221f7dfa
Jung Hyun, Nam <rkttu@rkttu.com>
2022-06-25 22:27:54 +0900

    IAppMessageBox 빌드 경고 수정

--- src/TableCloth/Contracts/IAppMessageBox.cs
+++ src/TableCloth/Contracts/IAppMessageBox.cs
@@ -3,12 +3,36 @@ using System.Windows;
 
 namespace TableCloth.Contracts
 {
+    /// <summary>
+    /// 메시지 상자를 표시하는 기능의 인터페이스를 정의합니다.
+    /// </summary>
     public interface IAppMessageBox
     {
-        MessageBoxResult DisplayInfo(object parentWindowHandle, string message, MessageBoxButton messageBoxButton = MessageBoxButton.OK);
+        /// <summary>
+        /// 정보를 안내하는 메시지 상자를 띄웁니다.
+        /// </summary>
+        /// <param name="parentWindowHandle">메시지 상자가 속한 부모 창의 핸들</param>
+        /// <param name="message">표시할 메시지</param>
+        /// <param name="messageBoxButton">메시지 박스 버튼 구성</param>
+        /// <returns>누른 버튼이 무엇인지 반환합니다.</returns>
+        MessageBoxResult DisplayInfo(object? parentWindowHandle, string message, MessageBoxButton messageBoxButton = MessageBoxButton.OK);
 
-        int DisplayError(object parentWindowHandle, Exception failureReason, bool isCritical);
+        /// <summary>
+        /// 오류를 안내하는 메시지 상자를 띄웁니다.
+        /// </summary>
+        /// <param name="parentWindowHandle">메시지 상자가 속한 부모 창의 핸들</param>
+        /// <param name="failureReason">발생한 예외 개체의 참조</param>
+        /// <param name="isCritical">심각성 여부</param>
+        /// <returns>누른 버튼이 무엇인지 반환합니다.</returns>
+        int DisplayError(object? parentWindowHandle, Exception failureReason, bool isCritical);
 
-        int DisplayError(object parentWindowHandle, string message, bool isCritical);
+        /// <summary>
+        /// 오류를 안내하는 메시지 상자를 띄웁니다.
+        /// </summary>
+        /// <param name="parentWindowHandle">메시지 상자가 속한 부모 창의 핸들</param>
+        /// <param name="message">표시할 메시지</param>
+        /// <param name="isCritical">심각성 여부</param>
+        /// <returns>누른 버튼이 무엇인지 반환합니다.</returns>
+        int DisplayError(object? parentWindowHandle, string message, bool isCritical);
     }
 }
--- src/TableCloth/Implementations/AppMessageBox.cs
+++ src/TableCloth/Implementations/AppMessageBox.cs
@@ -6,9 +6,19 @@ using TableCloth.Resources;
 
 namespace TableCloth.Implementations
 {
+    /// <summary>
+    /// Windows Presentation Foundation의 메시지 상자 표시 기능을 구현하는 구현체입니다.
+    /// </summary>
     public sealed class AppMessageBox : IAppMessageBox
     {
-        public MessageBoxResult DisplayInfo(object parentWindowHandle, string message, MessageBoxButton messageBoxButton = MessageBoxButton.OK)
+        /// <summary>
+        /// 정보를 안내하는 메시지 상자를 띄웁니다.
+        /// </summary>
+        /// <param name="parentWindowHandle">메시지 상자가 속한 부모 창의 핸들</param>
+        /// <param name="message">표시할 메시지</param>
+        /// <param name="messageBoxButton">메시지 박스 버튼 구성</param>
+        /// <returns>누른 버튼이 무엇인지 반환합니다.</returns>
+        public MessageBoxResult DisplayInfo(object? parentWindowHandle, string message, MessageBoxButton messageBoxButton = MessageBoxButton.OK)
         {
             var dispatcher = parentWindowHandle is Window window ? window?.Dispatcher : null;
 
@@ -26,10 +36,17 @@ namespace TableCloth.Implementations
                             message, StringResources.TitleText_Info,
                             messageBoxButton, MessageBoxImage.Information, MessageBoxResult.OK);
                 }),
-                new object[] { parentWindowHandle, message, });
+                new object?[] { parentWindowHandle, message, });
         }
 
-        public int DisplayError(object parentWindowHandle, Exception failureReason, bool isCritical)
+        /// <summary>
+        /// 오류를 안내하는 메시지 상자를 띄웁니다.
+        /// </summary>
+        /// <param name="parentWindowHandle">메시지 상자가 속한 부모 창의 핸들</param>
+        /// <param name="failureReason">발생한 예외 개체의 참조</param>
+        /// <param name="isCritical">심각성 여부</param>
+        /// <returns>누른 버튼이 무엇인지 반환합니다.</returns>
+        public int DisplayError(object? parentWindowHandle, Exception failureReason, bool isCritical)
         {
             var unwrappedException = failureReason;
 
@@ -39,7 +56,14 @@ namespace TableCloth.Implementations
             return DisplayError(parentWindowHandle, unwrappedException?.Message ?? StringResources.UnknownText, isCritical);
         }
 
-        public int DisplayError(object parentWindowHandle, string message, bool isCritical)
+        /// <summary>
+        /// 오류를 안내하는 메시지 상자를 띄웁니다.
+        /// </summary>
+        /// <param name="parentWindowHandle">메시지 상자가 속한 부모 창의 핸들</param>
+        /// <param name="message">표시할 메시지</param>
+        /// <param name="isCritical">심각성 여부</param>
+        /// <returns>누른 버튼이 무엇인지 반환합니다.</returns>
+        public int DisplayError(object? parentWindowHandle, string message, bool isCritical)
         {
             var dispatcher = parentWindowHandle is Window window ? window?.Dispatcher : null;
 
@@ -61,7 +85,7 @@ namespace TableCloth.Implementations
                             MessageBoxButton.OK,
                             isCritical ? MessageBoxImage.Stop : MessageBoxImage.Warning, MessageBoxResult.OK);
                 }),
-                new object[] { parentWindowHandle, message, isCritical });
+                new object?[] { parentWindowHandle, message, isCritical });
         }
     }
 }
