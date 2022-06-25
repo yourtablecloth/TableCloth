42120e624774510df1966b5ec1ee7834a05bab1c
Jung Hyun, Nam <rkttu@rkttu.com>
2022-06-25 22:36:23 +0900

    XML 주석 보충

--- src/TableCloth.Shared/Models/Configuration/PreferenceSettings.cs
+++ src/TableCloth.Shared/Models/Configuration/PreferenceSettings.cs
@@ -2,24 +2,54 @@
 
 namespace TableCloth.Models.Configuration
 {
+    /// <summary>
+    /// 식탁보의 설정을 나타냅니다.
+    /// </summary>
     public class PreferenceSettings
     {
+        /// <summary>
+        /// 호스트의 오디오 입력을 샌드박스 안으로 전달할지 여부를 나타냅니다.
+        /// </summary>
         public bool UseAudioRedirection { get; set; } = false;
 
+        /// <summary>
+        /// 호스트의 비디오 입력을 샌드박스 안으로 전달할지 여부를 나타냅니다.
+        /// </summary>
         public bool UseVideoRedirection { get; set; } = false;
 
+        /// <summary>
+        /// 샌드박스 안의 프린터 출력을 호스트의 프린터로 전달할지 여부를 나타냅니다.
+        /// </summary>
         public bool UsePrinterRedirection { get; set; } = false;
 
+        /// <summary>
+        /// 모두의 프린터 설치를 샌드박스 시작 직후 실행할지 여부를 나타냅니다.
+        /// </summary>
         public bool InstallEveryonesPrinter { get; set; } = true;
 
+        /// <summary>
+        /// Adobe Reader 설치를 샌드박스 시작 직후 실행할지 여부를 나타냅니다.
+        /// </summary>
         public bool InstallAdobeReader { get; set; } = true;
 
+        /// <summary>
+        /// 한컴오피스 뷰어 설치를 샌드박스 시작 직후 실행할지 여부를 나타냅니다.
+        /// </summary>
         public bool InstallHancomOfficeViewer { get; set; } = true;
 
+        /// <summary>
+        /// Internet Explorer 호환성 모드를 사용할지 여부를 나타냅니다.
+        /// </summary>
         public bool EnableInternetExplorerMode { get; set; } = true;
 
+        /// <summary>
+        /// 로그 수집 기능을 사용할지 여부를 나타냅니다.
+        /// </summary>
         public bool UseLogCollection { get; set; } = true;
 
+        /// <summary>
+        /// 식탁보 사용 시 주의 사항 동의 여부를 언제 확인했는지 시점을 기록합니다.
+        /// </summary>
         public DateTime? LastDisclaimerAgreedTime { get; set; } = null;
     }
 }
--- src/TableCloth.Shared/Models/Configuration/TableClothConfiguration.cs
+++ src/TableCloth.Shared/Models/Configuration/TableClothConfiguration.cs
@@ -4,19 +4,65 @@ using TableCloth.Models.Catalog;
 
 namespace TableCloth.Models.Configuration
 {
+    /// <summary>
+    /// 샌드박스 및 내부로 전달할 설정값을 저장하는 모델 클래스입니다.
+    /// </summary>
     [Serializable]
     public sealed class TableClothConfiguration
     {
+        /// <summary>
+        /// 선택한 X.509 인증서 정보입니다.
+        /// </summary>
         public X509CertPair CertPair { get; set; }
+
+        /// <summary>
+        /// 마이크 입력을 공유할 것인지 여부입니다.
+        /// </summary>
         public bool EnableMicrophone { get; set; }
+        
+        /// <summary>
+        /// 웹캠 입력을 공유할 것인지 여부입니다.
+        /// </summary>
         public bool EnableWebCam { get; set; }
+
+        /// <summary>
+        /// 프린터 출력을 공유할 것인지 여부입니다.
+        /// </summary>
         public bool EnablePrinters { get; set; }
+
+        /// <summary>
+        /// 모두의 프린터 설치를 샌드박스 시작 후 자동 실행할 지 여부입니다.
+        /// </summary>
         public bool EnableEveryonesPrinter { get; set; }
+
+        /// <summary>
+        /// Adobe Reader 설치를 샌드박스 시작 후 자동 실행할 지 여부입니다.
+        /// </summary>
         public bool EnableAdobeReader { get; set; }
+        
+        /// <summary>
+        /// 한컴오피스 뷰어 설치를 샌드박스 시작 후 자동 실행할 지 여부입니다.
+        /// </summary>
         public bool EnableHancomOfficeViewer { get; set; }
+
+        /// <summary>
+        /// Internet Explorer 호환성 모드를 사용할 것인지 여부입니다.
+        /// </summary>
         public bool EnableInternetExplorerMode { get; set; }
+
+        /// <summary>
+        /// 같이 설치하는 공통 소프트웨어에 관한 정보입니다.
+        /// </summary>
         public ICollection<CatalogCompanion> Companions { get; set; }
+
+        /// <summary>
+        /// 식탁보를 이용하여 샌드박스 내에서 설치해야 할 각 서비스 별 필요 소프트웨어에 관한 정보입니다.
+        /// </summary>
         public ICollection<CatalogInternetService> Services { get; set; }
+
+        /// <summary>
+        /// 샌드박스 명세 파일 (WSB) 및 데이터 파일을 저장할 디렉터리 경로입니다.
+        /// </summary>
         public string AssetsDirectoryPath { get; internal set; }
     }
 }
