cb863f6808a379fd99c6f5cb6a2a945f6b9dc5c3
ChJR <chjr8815@gmail.com>
2022-10-28 03:16:17 +0900

    리스트에 정렬 기능 추가
    검색 기능과도 같이 사용하는 것이 가능함.

---
+++ src/TableCloth/ExtendedGridViewColumn.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Controls;
+
+namespace TableCloth
+{
+    public class ExtendedGridViewColumn : GridViewColumn
+    {
+        public string BindingPath { get; set; }
+    }
+}
