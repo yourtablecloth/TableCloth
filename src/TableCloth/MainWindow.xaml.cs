780ac1e38f58635989d55dea0729dd72680da654
ChJR <chjr8815@gmail.com>
2022-10-28 03:16:17 +0900

    리스트에 정렬 기능 추가
    검색 기능과도 같이 사용하는 것이 가능함.

--- src/TableCloth/MainWindow.xaml.cs
+++ src/TableCloth/MainWindow.xaml.cs
@@ -626,5 +626,89 @@ namespace TableCloth.Implementations.WPF
 
             ViewModel.AppMessageBox.DisplayInfo(this, StringResources.Info_ShortcutSuccess);
         }
+
+        #region Sort Support
+
+        GridViewColumnHeader _lastHeaderClicked = null;
+        ListSortDirection? _lastDirection = null;
+
+        void GridViewColumnHeaderClickedHandler(object sender,
+                                                RoutedEventArgs e)
+        {
+            var headerClicked = e.OriginalSource as GridViewColumnHeader;
+            ListSortDirection? direction;
+
+            if (headerClicked != null)
+            {
+                if (headerClicked.Role != GridViewColumnHeaderRole.Padding)
+                {
+                    if (headerClicked != _lastHeaderClicked)
+                    {
+                        direction = ListSortDirection.Ascending;
+                    }
+                    else
+                    {
+                        if (_lastDirection == ListSortDirection.Ascending)
+                        {
+                            direction = ListSortDirection.Descending;
+                        }
+                        else if (_lastDirection == null)
+                        {
+                            direction = ListSortDirection.Ascending;
+                        }
+                        else
+                        {
+                            direction = null;
+                        }
+                    }
+
+                    //var columnBinding = headerClicked.Column.DisplayMemberBinding as Binding;
+                    //var sortBy = columnBinding?.Path.Path ?? headerClicked.Column.Header as string;
+                    var column = headerClicked.Column as ExtendedGridViewColumn;
+                    var sortBy = column?.BindingPath ?? headerClicked.Column.Header as string;
+
+                    Sort(sortBy, direction);
+
+                    if (direction == ListSortDirection.Ascending)
+                    {
+                        headerClicked.Column.HeaderTemplate =
+                          Resources["HeaderTemplateArrowUp"] as DataTemplate;
+                    }
+                    else if (direction == ListSortDirection.Descending)
+                    {
+                        headerClicked.Column.HeaderTemplate =
+                          Resources["HeaderTemplateArrowDown"] as DataTemplate;
+                    }
+                    else
+                    {
+                        headerClicked.Column.HeaderTemplate = null;
+                    }
+
+                    // Remove arrow from previously sorted header
+                    if (_lastHeaderClicked != null && _lastHeaderClicked != headerClicked)
+                    {
+                        _lastHeaderClicked.Column.HeaderTemplate = null;
+                    }
+
+                    _lastHeaderClicked = headerClicked;
+                    _lastDirection = direction;
+                }
+            }
+        }
+        private void Sort(string sortBy, ListSortDirection? direction)
+        {
+            ICollectionView dataView =
+              CollectionViewSource.GetDefaultView(SiteCatalog.ItemsSource);
+
+            dataView.SortDescriptions.Clear();
+            if (direction != null)
+            {
+                SortDescription sd = new SortDescription(sortBy, direction ?? ListSortDirection.Ascending);
+                dataView.SortDescriptions.Add(sd);
+            }
+            dataView.Refresh();
+        }
+
+        #endregion Sort Support
     }
 }
