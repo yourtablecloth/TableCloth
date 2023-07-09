af9e9fd10dfc3a8af7e1420527b632daa7b18241
Jung Hyun, Nam <rkttu@rkttu.com>
2023-07-09 17:28:17 +0900

    #107 Hostess에서 XML 파일 다운로드하는 부분에 retry logic을 추가함

--- src/Hostess/App.xaml.cs
+++ src/Hostess/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Threading;
 using System.Windows;
 using System.Xml;
 using System.Xml.Serialization;
@@ -15,122 +16,149 @@ namespace Hostess
     {
         private void Application_Startup(object sender, StartupEventArgs e)
         {
+            const int retryCount = 3;
+
             try
             {
-                using (var webClient = new WebClient())
+                for (int attemptCount = 1; attemptCount <= retryCount; attemptCount++)
                 {
                     CatalogDocument catalog = null;
+                    string lastModifiedValue = null;
 
                     try
                     {
+                        using (var webClient = new WebClient())
                         using (var catalogStream = webClient.OpenRead(StringResources.CatalogUrl))
                         {
-                            var lastModifiedValue = webClient.ResponseHeaders.Get("Last-Modified");
+                            lastModifiedValue = webClient.ResponseHeaders.Get("Last-Modified");
                             catalog = DeserializeFromXml<CatalogDocument>(catalogStream);
 
                             if (catalog == null)
                             {
                                 throw new XmlException(StringResources.HostessError_CatalogDeserilizationFailure);
                             }
-
-                            Current.InitCatalogDocument(catalog);
-                            Current.InitCatalogLastModified(lastModifiedValue);
                         }
                     }
                     catch (Exception ex)
                     {
-                        MessageBox.Show(StringResources.HostessError_CatalogLoadFailure(ex), StringResources.TitleText_Error,
-                            MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK);
-                        Current.Shutdown(0);
-                        return;
+                        catalog = null;
+                        Thread.Sleep(TimeSpan.FromSeconds(1.5d * attemptCount));
+
+                        if (attemptCount == retryCount)
+                        {
+                            MessageBox.Show(StringResources.HostessError_CatalogLoadFailure(ex), StringResources.TitleText_Error,
+                                MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK);
+                            Current.Shutdown(0);
+                            return;
+                        }
+
+                        continue;
                     }
 
+                    Current.InitCatalogDocument(catalog);
+                    Current.InitCatalogLastModified(lastModifiedValue);
+                    break;
+                }
+
+                for (int attemptCount = 1; attemptCount <= retryCount; attemptCount++)
+                {
                     IEModeListDocument ieModeList = null;
+                    string lastModifiedValue = null;
 
                     try
                     {
+                        using (var webClient = new WebClient())
                         using (var ieModeListStream = webClient.OpenRead(StringResources.IEModeListUrl))
                         {
-                            var lastModifiedValue = webClient.ResponseHeaders.Get("Last-Modified");
+                            lastModifiedValue = webClient.ResponseHeaders.Get("Last-Modified");
                             ieModeList = DeserializeFromXml<IEModeListDocument>(ieModeListStream);
 
-                            if (catalog == null)
+                            if (ieModeList == null)
                             {
                                 throw new XmlException(StringResources.HostessError_CatalogDeserilizationFailure);
                             }
-
-                            Current.InitIEModeListDocument(ieModeList);
-                            Current.InitIEModeListLastModified(lastModifiedValue);
                         }
                     }
                     catch (Exception ex)
                     {
-                        MessageBox.Show(StringResources.HostessError_CatalogLoadFailure(ex), StringResources.TitleText_Error,
-                            MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK);
-                        Current.Shutdown(0);
-                        return;
-                    }
+                        ieModeList = null;
+                        Thread.Sleep(TimeSpan.FromSeconds(1.5d * attemptCount));
 
-                    var targetSites = e.Args.Where(x => !x.StartsWith(StringResources.TableCloth_Switch_Prefix, StringComparison.Ordinal)).ToArray();
-                    Current.InitInstallSites(targetSites);
-
-                    var installEveryonesPrinter = false;
-                    var installAdobeReader = false;
-                    var installHancomOfficeViewer = false;
-                    var installRaiDrive = true;
-                    var hasIEModeEnabled = false;
-                    var showHelp = false;
-
-                    var options = e.Args.Where(x => x.StartsWith(StringResources.TableCloth_Switch_Prefix, StringComparison.Ordinal)).ToArray();
-                    foreach (var eachOption in options)
-                    {
-                        if (eachOption.StartsWith(StringResources.TableCloth_Switch_InstallEveryonesPrinter, StringComparison.Ordinal))
-                            installEveryonesPrinter = true;
-
-                        if (eachOption.StartsWith(StringResources.TableCloth_Switch_InstallAdobeReader, StringComparison.Ordinal))
-                            installAdobeReader = true;
-
-                        if (eachOption.StartsWith(StringResources.TableCloth_Switch_InstallHancomOfficeViewer, StringComparison.Ordinal))
-                            installHancomOfficeViewer = true;
-
-                        if (eachOption.StartsWith(StringResources.TableCloth_Switch_InstallRaiDrive, StringComparison.Ordinal))
-                            installRaiDrive = true;
-
-                        if (eachOption.StartsWith(StringResources.TableCloth_Switch_EnableIEMode, StringComparison.Ordinal))
-                            hasIEModeEnabled = true;
-
-                        if (eachOption.StartsWith(StringResources.TableCloth_Switch_Help, StringComparison.Ordinal))
-                            showHelp = true;
-                    }
-
-                    if (showHelp)
-                    {
-                        MessageBox.Show(StringResources.TableCloth_Hostess_Switches_Help, StringResources.TitleText_Info,
-                            MessageBoxButton.OK, MessageBoxImage.Information, MessageBoxResult.OK);
-                        Current.Shutdown(0);
-                        return;
-                    }
-
-                    Current.InitWillInstallEveryonesPrinter(installEveryonesPrinter);
-                    Current.InitWillInstallAdobeReader(installAdobeReader);
-                    Current.InitWillInstallHancomOfficeViewer(installHancomOfficeViewer);
-                    Current.InitWillInstallRaiDrive(installRaiDrive);
-                    Current.InitHasIEModeEnabled(hasIEModeEnabled);
-
-                    if (!targetSites.Any())
-                    {
-                        MessageBox.Show(StringResources.Hostess_No_Targets, StringResources.TitleText_Error,
-                            MessageBoxButton.OK, MessageBoxImage.Information, MessageBoxResult.OK);
-
-                        Process.Start(new ProcessStartInfo("https://www.naver.com/")
+                        if (attemptCount == retryCount)
                         {
-                            UseShellExecute = true,
-                            WindowStyle = ProcessWindowStyle.Maximized,
-                        });
+                            MessageBox.Show(StringResources.HostessError_CatalogLoadFailure(ex), StringResources.TitleText_Error,
+                                MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK);
+                            Current.Shutdown(0);
+                            return;
+                        }
 
-                        Current.Shutdown(0);
-                        return;
+                        continue;
                     }
+
+                    Current.InitIEModeListDocument(ieModeList);
+                    Current.InitIEModeListLastModified(lastModifiedValue);
+                    break;
+                }
+
+                var targetSites = e.Args.Where(x => !x.StartsWith(StringResources.TableCloth_Switch_Prefix, StringComparison.Ordinal)).ToArray();
+                Current.InitInstallSites(targetSites);
+
+                var installEveryonesPrinter = false;
+                var installAdobeReader = false;
+                var installHancomOfficeViewer = false;
+                var installRaiDrive = true;
+                var hasIEModeEnabled = false;
+                var showHelp = false;
+
+                var options = e.Args.Where(x => x.StartsWith(StringResources.TableCloth_Switch_Prefix, StringComparison.Ordinal)).ToArray();
+                foreach (var eachOption in options)
+                {
+                    if (eachOption.StartsWith(StringResources.TableCloth_Switch_InstallEveryonesPrinter, StringComparison.Ordinal))
+                        installEveryonesPrinter = true;
+
+                    if (eachOption.StartsWith(StringResources.TableCloth_Switch_InstallAdobeReader, StringComparison.Ordinal))
+                        installAdobeReader = true;
+
+                    if (eachOption.StartsWith(StringResources.TableCloth_Switch_InstallHancomOfficeViewer, StringComparison.Ordinal))
+                        installHancomOfficeViewer = true;
+
+                    if (eachOption.StartsWith(StringResources.TableCloth_Switch_InstallRaiDrive, StringComparison.Ordinal))
+                        installRaiDrive = true;
+
+                    if (eachOption.StartsWith(StringResources.TableCloth_Switch_EnableIEMode, StringComparison.Ordinal))
+                        hasIEModeEnabled = true;
+
+                    if (eachOption.StartsWith(StringResources.TableCloth_Switch_Help, StringComparison.Ordinal))
+                        showHelp = true;
+                }
+
+                if (showHelp)
+                {
+                    MessageBox.Show(StringResources.TableCloth_Hostess_Switches_Help, StringResources.TitleText_Info,
+                        MessageBoxButton.OK, MessageBoxImage.Information, MessageBoxResult.OK);
+                    Current.Shutdown(0);
+                    return;
+                }
+
+                Current.InitWillInstallEveryonesPrinter(installEveryonesPrinter);
+                Current.InitWillInstallAdobeReader(installAdobeReader);
+                Current.InitWillInstallHancomOfficeViewer(installHancomOfficeViewer);
+                Current.InitWillInstallRaiDrive(installRaiDrive);
+                Current.InitHasIEModeEnabled(hasIEModeEnabled);
+
+                if (!targetSites.Any())
+                {
+                    MessageBox.Show(StringResources.Hostess_No_Targets, StringResources.TitleText_Error,
+                        MessageBoxButton.OK, MessageBoxImage.Information, MessageBoxResult.OK);
+
+                    Process.Start(new ProcessStartInfo("https://www.naver.com/")
+                    {
+                        UseShellExecute = true,
+                        WindowStyle = ProcessWindowStyle.Maximized,
+                    });
+
+                    Current.Shutdown(0);
+                    return;
                 }
             }
             catch (Exception ex)
