01baa860edddf5923e0090b88a25fd577030a101
Jung Hyun, Nam <rkttu@rkttu.com>
2023-08-15 23:20:57 +0900

    #123 UI 뼈대를 만들기 위한 보일러플레이트 생성

---
+++ src/TableCloth/Pages/CatalogPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TableCloth.Pages
+{
+    /// <summary>
+    /// Interaction logic for CatalogPage.xaml
+    /// </summary>
+    public partial class CatalogPage : Page
+    {
+        public CatalogPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("Pages/DetailPage.xaml", UriKind.Relative));
+        }
+    }
+}
