7c29e796d0cba1b51df60ddbc3771982ffde1826
Jung Hyun, Nam <rkttu@rkttu.com>
2023-08-26 18:23:29 +0900

    검색 기능 추가

--- src/TableCloth/Pages/CatalogPage.xaml.cs
+++ src/TableCloth/Pages/CatalogPage.xaml.cs
@@ -26,6 +26,8 @@ namespace TableCloth.Pages
             InitializeComponent();
 
             var view = (CollectionView)CollectionViewSource.GetDefaultView(SiteCatalog.ItemsSource);
+            view.Filter = SiteCatalog_Filter;
+
             var groupDescription = new PropertyGroupDescription(nameof(CatalogInternetService.Category));
             view.GroupDescriptions.Add(groupDescription);
         }
@@ -33,6 +35,76 @@ namespace TableCloth.Pages
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             NavigationService.Navigate(new Uri("Pages/DetailPage.xaml", UriKind.Relative));
+        }
+
+        private bool SiteCatalog_Filter(object item)
+        {
+            var filterText = SiteCatalogFilter.Text;
+
+            if (string.IsNullOrWhiteSpace(filterText))
+                return true;
+
+            var actualItem = item as CatalogInternetService;
+
+            if (actualItem == null)
+                return true;
+
+            var splittedFilterText = filterText.Split(new char[] { ',', }, StringSplitOptions.RemoveEmptyEntries);
+            var result = false;
+
+            foreach (var eachFilterText in splittedFilterText)
+            {
+                result |= actualItem.DisplayName.Contains(eachFilterText, StringComparison.OrdinalIgnoreCase)
+                    || actualItem.CategoryDisplayName.Contains(eachFilterText, StringComparison.OrdinalIgnoreCase)
+                    || actualItem.Url.Contains(eachFilterText, StringComparison.OrdinalIgnoreCase)
+                    || actualItem.Packages.Count.ToString().Contains(eachFilterText, StringComparison.OrdinalIgnoreCase)
+                    || actualItem.Packages.Any(x => x.Name.Contains(eachFilterText, StringComparison.OrdinalIgnoreCase))
+                    || actualItem.Id.Contains(eachFilterText, StringComparison.OrdinalIgnoreCase);
+            }
+
+            return result;
+        }
+
+        private void SiteCatalogFilter_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            CollectionViewSource.GetDefaultView(SiteCatalog.ItemsSource).Refresh();
+        }
+
+        // https://stackoverflow.com/questions/660554/how-to-automatically-select-all-text-on-focus-in-wpf-textbox
+
+        private void SiteCatalogFilter_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            // Fixes issue when clicking cut/copy/paste in context menu
+            if (SiteCatalogFilter.SelectionLength < 1)
+                SiteCatalogFilter.SelectAll();
+        }
+
+        private void SiteCatalogFilter_LostTouchCapture(object sender, TouchEventArgs e)
+        {
+            // If user highlights some text, don't override it
+            if (SiteCatalogFilter.SelectionLength < 1)
+                SiteCatalogFilter.SelectAll();
+
+            // further clicks will not select all
+            SiteCatalogFilter.LostTouchCapture -= SiteCatalogFilter_LostTouchCapture;
+        }
+
+        private void SiteCatalogFilter_LostMouseCapture(object sender, MouseEventArgs e)
+        {
+            // If user highlights some text, don't override it
+            if (SiteCatalogFilter.SelectionLength < 1)
+                SiteCatalogFilter.SelectAll();
+
+            // further clicks will not select all
+            SiteCatalogFilter.LostMouseCapture -= SiteCatalogFilter_LostMouseCapture;
+        }
+
+
+        private void SiteCatalogFilter_LostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            // once we've left the TextBox, return the select all behavior
+            SiteCatalogFilter.LostMouseCapture += SiteCatalogFilter_LostMouseCapture;
+            SiteCatalogFilter.LostTouchCapture += SiteCatalogFilter_LostTouchCapture;
         }
     }
 }
