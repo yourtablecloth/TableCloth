362f9782d29aa6dbc0206cc2c512820634d33c1f
Jung Hyun, Nam <rkttu@rkttu.com>
2023-08-26 20:41:11 +0900

    페이지 탐색 애니메이션 추가

--- src/TableCloth/MainWindowV2.xaml.cs
+++ src/TableCloth/MainWindowV2.xaml.cs
@@ -1,16 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+using System.Windows.Navigation;
+using System.Windows.Threading;
 
 namespace TableCloth
 {
@@ -22,6 +15,68 @@ namespace TableCloth
         public MainWindowV2()
         {
             InitializeComponent();
+        }
+
+        // https://stackoverflow.com/questions/2135113/how-do-you-do-transition-effects-using-the-frame-control-in-wpf
+        private bool _allowDirectNavigation = true;
+        private NavigatingCancelEventArgs _navArgs = null;
+        private double _oldValue = 0d;
+
+        private readonly Duration _duration = new Duration(TimeSpan.FromSeconds(0.25));
+        private readonly DependencyProperty _targetProperty = OpacityProperty;
+
+        private void PageFrame_Navigating(object sender, NavigatingCancelEventArgs e)
+        {
+            if (Content == null || _allowDirectNavigation)
+            {
+                _allowDirectNavigation = false;
+                return;
+            }
+
+            e.Cancel = true;
+            _navArgs = e;
+
+            var animation0 = new DoubleAnimation();
+            animation0.From = _oldValue = PageFrame.Opacity;
+            animation0.To = 0;
+            animation0.Duration = _duration;
+            animation0.Completed += SlideCompleted;
+
+            PageFrame.BeginAnimation(_targetProperty, animation0);
+        }
+
+        private void SlideCompleted(object sender, EventArgs e)
+        {
+            _allowDirectNavigation = true;
+            switch (_navArgs.NavigationMode)
+            {
+                case NavigationMode.New:
+                    if (_navArgs.Uri == null)
+                        PageFrame.Navigate(_navArgs.Content);
+                    else
+                        PageFrame.Navigate(_navArgs.Uri);
+                    break;
+                case NavigationMode.Back:
+                    PageFrame.GoBack();
+                    break;
+                case NavigationMode.Forward:
+                    PageFrame.GoForward();
+                    break;
+                case NavigationMode.Refresh:
+                    PageFrame.Refresh();
+                    break;
+            }
+
+            Dispatcher.BeginInvoke(DispatcherPriority.Loaded,
+                (ThreadStart)delegate ()
+                {
+                    var animation0 = new DoubleAnimation();
+                    animation0.From = 0;
+                    animation0.To = _oldValue;
+                    animation0.Duration = _duration;
+
+                    PageFrame.BeginAnimation(_targetProperty, animation0);
+                });
         }
     }
 }
