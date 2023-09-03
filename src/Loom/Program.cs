477300861db31e897d495a473004aa81245ab640
Jung Hyun, Nam <rkttu@rkttu.com>
2023-09-03 15:13:32 +0900

    Loom 프로젝트 추가

---
+++ src/Loom/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+Synopsis
+
+wsbx.exe
+  build
+    --virtual-gpu [yes | no]
+    --networking [yes | no]
+    --map [host] [sandbox] [readonly]
+    --startup-script [file path]
+    --audio-input [yes | no]
+    --video-input [yes | no]
+    --protected-client [yes | no]
+    --printer-redirect [yes | no]
+    --clipboard-redirect [yes | no]
+    --memory [size in mb]
+    --add [host]
+    [--force]
+    output [zip file path]
+
+  run
+    --working-directory [directory path]
+    --no-wait
+    zip file path
+
+  help
+  version
+ */
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+    }
+}
