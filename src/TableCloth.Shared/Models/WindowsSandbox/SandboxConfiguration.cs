d0088e875b4a8f5092128c4bfa7f43923f94ce70
Jung Hyun Nam <rkttu@rkttu.com>
2023-09-05 22:51:37 +0900

    SandboxConfiguration에서 null 값이 지정된 경우 element 렌더링을 건너뛰게 Specified 속성을 추가함

--- src/TableCloth.Shared/Models/WindowsSandbox/SandboxConfiguration.cs
+++ src/TableCloth.Shared/Models/WindowsSandbox/SandboxConfiguration.cs
@@ -7,19 +7,64 @@ namespace TableCloth.Models.WindowsSandbox
     [XmlRoot("Configuration")]
     public sealed partial class SandboxConfiguration
     {
+        [XmlElement("Networking")]
+        public string Networking { get; set; }
+
+        [XmlIgnore]
+        public bool NetworkingSpecified => Networking != null;
+
         [XmlElement("AudioInput")]
         public string AudioInput { get; set; }
+
+        [XmlIgnore]
+        public bool AudioInputSpecified => AudioInput != null;
 
         [XmlElement("VideoInput")]
         public string VideoInput { get; set; }
 
+        [XmlIgnore]
+        public bool VideoInputSpecified => VideoInput != null;
+
+        [XmlElement("vGPU")]
+        public string VirtualGpu { get; set; }
+
+        [XmlIgnore]
+        public bool VirtualGpuSpecified => VirtualGpu != null;
+
         [XmlElement("PrinterRedirection")]
         public string PrinterRedirection { get; set; }
+
+        [XmlIgnore]
+        public bool PrinterRedirectionSpecified => PrinterRedirection != null;
+
+        [XmlElement("ClipboardRedirection")]
+        public string ClipboardRedirection { get; set; }
+
+        [XmlIgnore]
+        public bool ClipboardRedirectionSpecified => ClipboardRedirection != null;
+
+        [XmlElement("ProtectedClient")]
+        public string ProtectedClient { get; set; }
+
+        [XmlIgnore]
+        public bool ProtectedClientSpecified => ProtectedClient != null;
 
         [XmlArray, XmlArrayItem(typeof(string), ElementName = "Command")]
         public List<string> LogonCommand { get; set; } = new List<string>();
 
+        [XmlIgnore]
+        public bool LogonCommandSpecified => LogonCommand != null && LogonCommand.Count > 0;
+
         [XmlArray, XmlArrayItem(typeof(SandboxMappedFolder), ElementName = "MappedFolder")]
         public List<SandboxMappedFolder> MappedFolders { get; } = new List<SandboxMappedFolder>();
+
+        [XmlIgnore]
+        public bool MappedFoldersSpecified => MappedFolders != null && MappedFolders.Count > 0;
+
+        [XmlElement("MemoryInMB")]
+        public int? MemoryInMB { get; set; }
+
+        [XmlIgnore]
+        public bool MemoryInMBSpecified => MemoryInMB.HasValue;
     }
 }
