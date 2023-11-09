c5bf5ea2574de0637a026dfd1747e9ca33f03576
Jung Hyun, Nam <rkttu@rkttu.com>
2023-11-10 00:18:19 +0900

    xUnit 기반 유닛 테스트 초안 추가

---
+++ src/TableCloth.Test/ContainerFixture.cs
@@ -0,0 +1,24 @@
+namespace TableCloth.Test;
+
+using Microsoft.Extensions.DependencyInjection;
+using TableCloth.Components;
+using TableCloth.Resources;
+
+public class ContainerFixture
+{
+    public ContainerFixture()
+    {
+        var svcCollection = new ServiceCollection();
+        svcCollection
+            .AddLogging()
+            .AddHttpClient(nameof(TableCloth), c =>
+            {
+                c.DefaultRequestHeaders.Add("User-Agent", StringResources.UserAgentText);
+            });
+
+        svcCollection.AddSingleton<ResourceResolver>();
+        ServiceProvider = svcCollection.BuildServiceProvider();
+    }
+
+    public ServiceProvider ServiceProvider { get; private set; }
+}
---
+++ src/TableCloth.Test/ResourceResolverTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TableCloth.Components;
+
+namespace TableCloth.Test
+{
+    public class ResourceResolverTest : IClassFixture<ContainerFixture>
+    {
+        public ResourceResolverTest(ContainerFixture fixture)
+        {
+            serviceProvider = fixture.ServiceProvider;
+            resourceResolver = serviceProvider.GetService<ResourceResolver>() ??
+                throw new Exception("Cannot obtain resource resolver due to configuration");
+        }
+
+        private IServiceProvider serviceProvider;
+        private ResourceResolver resourceResolver;
+
+        [Fact]
+        public async Task TestGetLatestVersion()
+        {
+            // Arrange
+            const string repoOwner = "yourtablecloth";
+            const string repoName = "TableCloth";
+
+            // Act
+            var result = await resourceResolver.GetLatestVersion(repoOwner, repoName);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.NotEmpty(result);
+        }
+    }
+}
