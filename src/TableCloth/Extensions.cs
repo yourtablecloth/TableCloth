249318b3fba822a216048bda9f90b9b8faf53f53
Jung Hyun Nam <rkttu@rkttu.com>
2023-12-15 17:37:10 +0900

    기타 사용되지 않는 코드 제거

--- src/TableCloth/Extensions.cs
+++ src/TableCloth/Extensions.cs
@@ -7,9 +7,5 @@ namespace TableCloth
     {
         public static HttpClient CreateTableClothHttpClient(this IHttpClientFactory httpClientFactory)
             => httpClientFactory!.CreateClient(nameof(TableCloth));
-
-        public static T AssignService<T>(this IServiceProvider serviceProvider, out T target)
-            where T : class
-            => target = serviceProvider.GetService(typeof(T)) as T;
     }
 }
