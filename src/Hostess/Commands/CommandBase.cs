566288b30c1be5a38e0902f1a5ab60dd382a175e
Jung Hyun Nam <rkttu@rkttu.com>
2023-12-20 15:49:35 +0900

    Hostess에도 CommandBase를 적용함

---
+++ src/Hostess/Commands/CommandBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Hostess.Commands
+{
+    internal abstract class CommandBase : ICommand
+    {
+        private bool _canExecute = true;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            this._canExecute = this.EvaluateCanExecute();
+            CommandManager.InvalidateRequerySuggested();
+        }
+
+        protected virtual bool EvaluateCanExecute()
+            => true;
+
+        public bool CanExecute(object parameter)
+            => _canExecute;
+
+        public abstract void Execute(object parameter);
+    }
+}
--- src/Hostess/Commands/ShowErrorMessageCommand.cs
+++ src/Hostess/Commands/ShowErrorMessageCommand.cs
@@ -1,25 +1,12 @@
-﻿using System;
-using System.Windows;
-using System.Windows.Input;
+﻿using System.Windows;
 using TableCloth.Resources;
 
 namespace Hostess.Commands
 {
-    internal sealed class ShowErrorMessageCommand : ICommand
+    internal sealed class ShowErrorMessageCommand : CommandBase
     {
-        private EventHandler _canExecuteChanged;
-
-        public event EventHandler CanExecuteChanged
-        {
-            add => _canExecuteChanged += value;
-            remove => _canExecuteChanged -= value;
-        }
-
-        public bool CanExecute(object parameter)
-            => true;
-
-        public void Execute(object parameter) =>
-            MessageBox.Show(
+        public override void Execute(object parameter)
+            => MessageBox.Show(
                 StringResources.HostessError_PackageInstallFailure(parameter as string), StringResources.AppName,
                 MessageBoxButton.OK, MessageBoxImage.Information, MessageBoxResult.OK);
     }
