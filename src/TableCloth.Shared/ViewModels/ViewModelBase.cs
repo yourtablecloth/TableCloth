e2c29a5154ab5ae307fadecff0c0a142a072c41e
Jung Hyun Nam <rkttu@rkttu.com>
2023-12-29 23:36:21 +0900

    ViewModelBase를 공유 프로젝트로 이동

---
+++ src/TableCloth.Shared/ViewModels/ViewModelBase.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace TableCloth.ViewModels
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler
+#if !NETFX
+            ?
+#endif
+            PropertyChanged;
+
+        protected void NotifyPropertyChanged(
+            [CallerMemberName] string
+#if !NETFX
+            ?
+#endif
+            propertyName = default)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        protected void NotifyMultiplePropertiesChanged(
+            string[] propertiesToNotify)
+        {
+            if (propertiesToNotify == null)
+                return;
+
+            foreach (var eachPropertyName in propertiesToNotify)
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(eachPropertyName ?? string.Empty));
+        }
+
+        protected virtual bool SetProperty<T>(
+            ref T member, T value,
+            [CallerMemberName] string
+#if !NETFX
+            ?
+#endif
+            propertyName = default)
+        {
+            if (EqualityComparer<T>.Default.Equals(member, value))
+                return false;
+
+            member = value;
+            NotifyPropertyChanged(propertyName);
+            return true;
+        }
+
+        protected virtual bool SetProperty<T>(
+            ref T member, T value,
+            string[] propertiesToNotify)
+        {
+            if (EqualityComparer<T>.Default.Equals(member, value))
+                return false;
+
+            member = value;
+            NotifyMultiplePropertiesChanged(propertiesToNotify);
+            return true;
+        }
+    }
+}
