e45fb93fbf2ecccfda02a8ecf20e24730707f688
Jung Hyun Nam <rkttu@rkttu.com>
2023-12-29 16:18:48 +0900

    CommandLineArgumentModel을 Shared 프로젝트로 이동

---
+++ src/TableCloth.Shared/Models/CommandLineArgumentModel.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TableCloth.Models
+{
+    public sealed class CommandLineArgumentModel
+    {
+        public CommandLineArgumentModel(
+            string[]
+#if !NETFX
+            ?
+#endif
+            selectedServices = default,
+            bool? enableMicrophone = default,
+            bool? enableWebCam = default,
+            bool? enablePrinters = default,
+            
+            string
+#if !NETFX
+            ?
+#endif
+            certPrivateKeyPath = default,
+
+            string
+#if !NETFX
+            ?
+#endif
+            certPublicKeyPath = default,
+
+            bool? installEveryonesPrinter = default,
+            bool? installAdobeReader = default,
+            bool? installHancomOfficeViewer = default,
+            bool? installRaiDrive = default,
+            bool? enableInternetExplorerMode = default,
+            bool showCommandLineHelp = default)
+        {
+            SelectedServices = selectedServices ?? Enumerable.Empty<string>();
+            EnableMicrophone = enableMicrophone;
+            EnableWebCam = enableWebCam;
+            EnablePrinters = enablePrinters;
+            CertPrivateKeyPath = certPrivateKeyPath;
+            CertPublicKeyPath = certPublicKeyPath;
+            InstallEveryonesPrinter = installEveryonesPrinter;
+            InstallAdobeReader = installAdobeReader;
+            InstallHancomOfficeViewer = installHancomOfficeViewer;
+            InstallRaiDrive = installRaiDrive;
+            EnableInternetExplorerMode = enableInternetExplorerMode;
+            ShowCommandLineHelp = showCommandLineHelp;
+        }
+
+        public bool? EnableMicrophone { get; private set; }
+
+        public bool? EnableWebCam { get; private set; }
+
+        public bool? EnablePrinters { get; private set; }
+
+        public string
+#if !NETFX
+            ?
+#endif
+            CertPrivateKeyPath { get; private set; }
+
+        public string
+#if !NETFX
+            ?
+#endif
+            CertPublicKeyPath { get; private set; }
+
+        public bool? InstallEveryonesPrinter { get; private set; }
+
+        public bool? InstallAdobeReader { get; private set; }
+
+        public bool? InstallHancomOfficeViewer { get; private set; }
+
+        public bool? InstallRaiDrive { get; private set; }
+
+        public bool? EnableInternetExplorerMode { get; private set; }
+
+        public bool ShowCommandLineHelp { get; private set; }
+
+        public IEnumerable<string> SelectedServices { get; private set; } = new List<string>();
+    }
+}
