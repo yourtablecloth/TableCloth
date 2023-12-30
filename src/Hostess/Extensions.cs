ee9b0125a254cd10ad0f6936b7b155d1e809c0ad
Jung Hyun Nam <rkttu@rkttu.com>
2023-12-30 23:54:02 +0900

    Hostess - 사용하지 않는 Extensions 코드 제거

--- src/Hostess/Extensions.cs
+++ src/Hostess/Extensions.cs
@@ -17,12 +17,6 @@ namespace Hostess
             return httpClientFactory.CreateClient(nameof(TableCloth));
         }
 
-        public static void AddCommands(this IServiceCollection services, params Type[] commandTypes)
-        {
-            foreach (var eachCommandType in commandTypes)
-                services.AddSingleton(eachCommandType);
-        }
-
         public static IServiceCollection AddWindow<TWindow, TViewModel>(this IServiceCollection services,
             Func<IServiceProvider, TWindow> windowImplementationFactory = default,
             Func<IServiceProvider, TViewModel> viewModelImplementationFactory = default)
@@ -40,51 +34,6 @@ namespace Hostess
                 services.AddTransient<TViewModel>();
 
             return services;
-        }
-
-        public static IServiceCollection AddPage<TPage, TViewModel>(this IServiceCollection services,
-            bool addPageAsSingleton = false,
-            Func<IServiceProvider, TPage> pageImplementationFactory = default,
-            Func<IServiceProvider, TViewModel> viewModelImplementationFactory = default)
-            where TPage : Page
-            where TViewModel : class
-        {
-            if (addPageAsSingleton)
-            {
-                if (pageImplementationFactory != null)
-                    services.AddSingleton<TPage>(pageImplementationFactory);
-                else
-                    services.AddSingleton<TPage>();
-            }
-            else
-            {
-                if (pageImplementationFactory != null)
-                    services.AddTransient<TPage>(pageImplementationFactory);
-                else
-                    services.AddTransient<TPage>();
-            }
-
-            if (viewModelImplementationFactory != null)
-                services.AddTransient<TViewModel>(viewModelImplementationFactory);
-            else
-                services.AddTransient<TViewModel>();
-
-            return services;
-        }
-
-        // https://stackoverflow.com/a/2914599
-        public static bool TryLeaveFocus(this FrameworkElement targetElement)
-        {
-            if (targetElement == null)
-                return false;
-
-            var parent = (FrameworkElement)targetElement.Parent;
-            while (parent != null && parent is IInputElement && !((IInputElement)parent).Focusable)
-                parent = (FrameworkElement)parent.Parent;
-
-            var scope = FocusManager.GetFocusScope(targetElement);
-            FocusManager.SetFocusedElement(scope, parent as IInputElement);
-            return true;
         }
     }
 }
