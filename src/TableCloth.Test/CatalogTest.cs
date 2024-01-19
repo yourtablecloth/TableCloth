b8ca4f810ed5c09c8ce1e41ed50feaa7166fa5e5
Jung Hyun Nam <rkttu@rkttu.com>
2024-01-19 22:47:15 +0900

    카탈로그 유효성 검사를 위한 테스트 코드 추가

---
+++ src/TableCloth.Test/CatalogTest.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using TableCloth.Components;
+
+namespace TableCloth.Test;
+
+public class CatalogTest
+{
+    public CatalogTest()
+    {
+        _testHost = TableClothApp.CreateHostBuilder(
+            servicesBuilderOverride: services => services
+                .ProvideMockupApplication()
+                .ReplaceWithMock<IMessageBoxService>()
+            ).Build();
+    }
+
+    private readonly IHost _testHost;
+
+    [Fact]
+    public void ValidateCatalog()
+    {
+        /*
+        // given
+        var sut = _testHost.Services.GetRequiredService<ICatalogDeserializer>();
+        var targetFilePath = @"E:\Projects\TableClothCatalog\docs\Catalog.xml";
+
+        // when
+        var catalog = sut.Deserialize(File.OpenText(targetFilePath));
+
+        // then
+        Assert.NotNull(catalog);
+        */
+    }
+}
