71586561a05ec251e1d87acfac2e485f82728404
Jung Hyun Nam <rkttu@rkttu.com>
2024-01-25 17:06:07 +0900

    App.xaml에서 Main 메서드 분리

---
+++ src/Hostess/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Hostess
+{
+    internal static class Program
+    {
+        [STAThread]
+        private static void Main()
+        {
+            RunApp();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+        private static void RunApp()
+        {
+            var app = new App();
+            app.Run();
+        }
+    }
+}
---
+++ src/TableCloth/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace TableCloth;
+
+internal static class Program
+{
+    [STAThread]
+    private static void Main()
+    {
+        RunApp();
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+    private static void RunApp()
+    {
+        var app = new App();
+        app.Run();
+    }
+}
