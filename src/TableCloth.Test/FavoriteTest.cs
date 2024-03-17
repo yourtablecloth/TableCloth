96b939618b20a987e93421b99b86c5db47a7371d
jonghoon <jonghoon.1738@gmail.com>
2024-03-17 13:36:55 +0900

    Favorte Save Test

---
+++ src/TableCloth.Test/FavoriteTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TableCloth.Commands.CatalogPage;
+using TableCloth.Components.Implementations;
+using TableCloth.Components;
+using TableCloth.Models.Catalog;
+using TableCloth.Models.Configuration;
+
+namespace TableCloth.Test;
+
+public class FavoriteTest
+{
+    // OnFavorite
+    [Fact]
+    public async Task CatalogPageItemFavoriteCommandTest()
+    {
+        //arrange
+        var mockPreferencesManager = new Mock<IPreferencesManager>();
+        var defaultSettings = new PreferenceSettings
+        {
+            Favorites =new List<string>()
+        };
+        mockPreferencesManager.Setup(x => x.LoadPreferencesAsync(It.IsAny<CancellationToken>()))
+            .ReturnsAsync(defaultSettings);
+        mockPreferencesManager.Setup(x => x.SavePreferencesAsync(defaultSettings, It.IsAny<CancellationToken>()));
+
+        CatalogInternetService service = new()
+        {
+            Id = "Service1",
+            IsFavorite = true,
+        };
+        var command = new CatalogPageItemFavoriteCommand(mockPreferencesManager.Object);
+        
+        //act
+        await command.ExecuteAsync(service);
+
+        //assert
+        var savedCountIsOne = defaultSettings.Favorites.Count == 1;
+        Assert.True(savedCountIsOne);
+    }
+
+    [Fact]
+    public async Task LoadCatalogPageItems()
+    {
+
+    }
+}
