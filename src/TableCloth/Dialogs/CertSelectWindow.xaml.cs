2435d03a8f0fd77ad98d274c8a5d33a54d6241a6
Jung Hyun, Nam <rkttu@rkttu.com>
2024-04-05 14:52:49 +0900

    #190 인증서 선택 대화 상자에서 항목을 더블클릭하면 선택될 수 있게 기능 보강

--- src/TableCloth/Dialogs/CertSelectWindow.xaml.cs
+++ src/TableCloth/Dialogs/CertSelectWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows;
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Windows;
 using TableCloth.Events;
+using TableCloth.Models.Configuration;
 using TableCloth.ViewModels;
 
 namespace TableCloth.Dialogs;
@@ -21,5 +23,16 @@ public partial class CertSelectWindow : Window
     {
         this.DialogResult = e.DialogResult;
         this.Close();
+    }
+
+    private void ListView_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+    {
+        switch (e.OriginalSource)
+        {
+            case FrameworkElement fe when fe.DataContext is X509CertPair && ViewModel.SelectedCertPair != null:
+                DialogResult = true;
+                Close();
+                break;
+        }
     }
 }
