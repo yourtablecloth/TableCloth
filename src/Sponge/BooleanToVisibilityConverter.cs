42ad4b46dce0db2c4b91d84c4423be785c4c75f2
Jung Hyun Nam <rkttu@rkttu.com>
2024-05-10 18:10:41 +0900

    #209 - Sponge 애플리케이션 초안 구현 (with dotEnhancer)

---
+++ src/Sponge/BooleanToVisibilityConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Sponge
+{
+    [ValueConversion(typeof(bool), typeof(Visibility))]
+    public sealed class BooleanToVisibilityConverter : IValueConverter
+    {
+        public Visibility TrueValue { get; set; } = Visibility.Visible;
+        public Visibility FalseValue { get; set; } = Visibility.Collapsed;
+
+        public BooleanToVisibilityConverter()
+        {
+            // set defaults
+            TrueValue = Visibility.Visible;
+            FalseValue = Visibility.Collapsed;
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is bool))
+                return null;
+            return (bool)value ? TrueValue : FalseValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (Equals(value, TrueValue))
+                return true;
+            if (Equals(value, FalseValue))
+                return false;
+            return null;
+        }
+    }
+}
---
+++ src/Sponge/InverseBooleanConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Sponge
+{
+    [ValueConversion(typeof(bool), typeof(bool))]
+    public sealed class InverseBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (targetType != typeof(bool))
+                throw new InvalidOperationException("The target must be a boolean");
+
+            return !(bool)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotSupportedException();
+    }
+}
