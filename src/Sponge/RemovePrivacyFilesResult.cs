e3d49cbcfc046878aed250eead94b93f17942f26
Jung Hyun, Nam <rkttu@rkttu.com>
2024-05-12 21:23:13 +0900

    #209 - 코드 테스트 후 실행 시 문제가 있는 부분을 고침

---
+++ src/Sponge/RemovePrivacyFilesResult.cs
@@ -0,0 +1,17 @@
+﻿namespace Sponge
+{
+    public class RemovePrivacyFilesResult
+    {
+        public RemovePrivacyFilesResult(int succeedFileCount, int failedFileCount)
+        {
+            SucceedFileCount = succeedFileCount;
+            FailedFileCount = failedFileCount;
+        }
+
+        public int SucceedFileCount { get; } = 0;
+        public int FailedFileCount { get; } = 0;
+        public int TotalFileCount => SucceedFileCount + FailedFileCount;
+
+        public override string ToString() => $"Succeed: {SucceedFileCount}, Failed: {FailedFileCount}";
+    }
+}
