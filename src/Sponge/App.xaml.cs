0724f40751758c225ee2b3961c6a596c56b195b5
Jung Hyun, Nam <rkttu@rkttu.com>
2024-05-13 10:09:51 +0900

    #209 - Windows Sandbox 환경에서만 실행되도록 조건을 검사함. 디버그 빌드에서만 계속 실행을 허용.

--- src/Sponge/App.xaml.cs
+++ src/Sponge/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using TableCloth;
 
 namespace Sponge
 {
@@ -13,5 +14,16 @@ namespace Sponge
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            if (!Helpers.SandboxAccountNames.Contains(Environment.UserName, StringComparer.OrdinalIgnoreCase))
+            {
+                MessageBox.Show("이 프로그램은 Windows Sandbox 환경에서만 실행하도록 설계되었습니다.", "알림", MessageBoxButton.OK, MessageBoxImage.Warning);
+
+#if !DEBUG
+                Shutdown();
+#endif // !DEBUG
+            }
+        }
     }
 }
