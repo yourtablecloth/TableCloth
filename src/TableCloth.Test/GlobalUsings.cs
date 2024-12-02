49f2b7f850379f29ff1f453d634cc9be7fcfb3e4
Jung Hyun, Nam <rkttu@rkttu.com>
2024-12-02 19:54:21 +0900

    단위 테스트 관련 코드 작성을 거의 하고 있지 않아 빌드 속도 개선을 위해 제거함

--- src/TableCloth.Test/GlobalUsings.cs
+++
@@ -1,8 +0,0 @@
-global using Microsoft.Extensions.DependencyInjection;
-global using Microsoft.Extensions.DependencyInjection.Extensions;
-global using Microsoft.Extensions.Hosting;
-global using Moq;
-global using System.Windows;
-global using TableCloth.Resources;
-global using Xunit;
-global using static Moq.It;
