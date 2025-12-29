39e2cec7e1dff6e32df5845c6e9bdb5f50d8426d
Jung Hyun, Nam <rkttu@rkttu.com>
2025-12-29 11:37:26 +0900

    CommandLineArguments 단위 테스트 추가
    CommandLineArguments 클래스에 대한 단위 테스트 파일(CommandLineArgumentsTests.cs)을 추가했습니다. 생성자, GetHelpStringAsync, GetCurrent 메서드의 동작을 검증하는 세 개의 테스트 메서드를 포함하고 있습니다. MSTest 프레임워크를 사용하여 테스트가 작성되었습니다.

---
+++ src/Spork.Test/CommandLineArgumentsTests.cs
@@ -0,0 +1,34 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Spork.Components;
+using Spork.Components.Implementations;
+using System.Threading.Tasks;
+
+namespace Spork.Test
+{
+    [TestClass]
+    public class CommandLineArgumentsTests
+    {
+        [TestMethod]
+        public void Constructor_ShouldInitializeOptions()
+        {
+            var args = new CommandLineArguments();
+            Assert.IsNotNull(args);
+        }
+
+        [TestMethod]
+        public async Task GetHelpStringAsync_ShouldReturnNonEmptyString()
+        {
+            var args = new CommandLineArguments();
+            var help = await args.GetHelpStringAsync();
+            Assert.IsFalse(string.IsNullOrEmpty(help));
+        }
+
+        [TestMethod]
+        public void GetCurrent_ShouldReturnModel()
+        {
+            var args = new CommandLineArguments();
+            var model = args.GetCurrent();
+            Assert.IsNotNull(model);
+        }
+    }
+}
